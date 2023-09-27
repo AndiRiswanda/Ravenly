3e60af23ec3d543bca8af760707a4d7969d08f6d
AndiRiswanda <andiriswandalah.gmail@com>
2023-09-27 12:10:52 +0800

    mengcompress beberapa line of code

--- Tugas lab/week3/P3_2_H01231008.py
+++ Tugas lab/week3/P3_2_H01231008.py
@@ -1,12 +1,11 @@
 harga = int(input("Harga barang : "))
-tunai = int(input("Uang pembeli: "))
+tunai = int(input("Uang pembeli: ")) - harga
 
-baki = tunai - harga
 p = [100000,50000,20000,10000,5000,2000,1000]
 o = [0]*7
 
 for i in range (len(p)):
-    while baki - p[i] >= 0:
+    while tunai - p[i] >= 0:
         o[i] += 1
-        baki -= p[i]
+        tunai -= p[i]
     print (f"{o[i]} uang {p[i]}")
